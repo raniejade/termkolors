9d49cb0ac8bb79abb54943ce242ca61dec7581fd
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2019-03-03 10:51:24 +1100

    Add console reporter sample

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,8 +1,12 @@
 import ansi4k.Term256
 import ansi4k.invoke
+import ansi4k.plus
+
+const val INDENT = "  "
 
 fun main() {
     with(Term256) {
+        println("# 256 colors")
         for (i in 0..15) {
             for (j in 0..15) {
                 val code = i * 16 + j
@@ -10,5 +14,22 @@ fun main() {
             }
             println()
         }
+        println()
+
+        val section = (color(255))
+        val pass = green
+        val skipped = yellow
+        val failed = red
+
+        println("# Console reporter for a test framework")
+        println((section + underline)("START"))
+        println(INDENT + section("A calculator"))
+        println(INDENT.repeat(2) + section("addition"))
+        println(INDENT.repeat(3) + pass("\u2713$INDENT" + "1 + 2 == 3"))
+        println(INDENT.repeat(3) + pass("\u2713$INDENT" + "1 + -1 == 0"))
+        println((section + underline)("SUMMARY:"))
+        println(pass("\u2713 2 tests completed"))
+        println(skipped("\u2726 0 tests skipped"))
+        println(failed("\u2717 0 tests failed"))
     }
 }
